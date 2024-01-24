e748de26cf0b2a112776b4baccced36c63fa01e9
Jan Zwiener <jan@zwiener.org>
2024-01-24 23:45:03 +0100

    Changed neural network size to 256

--- src/nnpolicynetwork.py
+++ src/nnpolicynetwork.py
@@ -5,8 +5,8 @@ class NNPolicyNetwork(nn.Module):
     def __init__(self, input_size, output_size):
         super(NNPolicyNetwork, self).__init__()
         self.fc1 = nn.Linear(input_size, 128)  # First MLP layer
-        self.fc2 = nn.Linear(128, 128)         # Second MLP layer
-        self.fc3 = nn.Linear(128, output_size) # Output layer
+        self.fc2 = nn.Linear(128, 256)         # Second MLP layer
+        self.fc3 = nn.Linear(256, output_size) # Output layer
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
