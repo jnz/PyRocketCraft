db7477a445fc9709b3c77ed13334eb0f7df12b0c
Jan Zwiener <jan@zwiener.org>
2024-01-26 23:20:02 +0100

    Fix: Don't overwrite existing LIBRARY_PATH

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  export DYLD_LIBRARY_PATH="$CURRENT_SCRIPT_DIR/acados/lib"
+  export DYLD_LIBRARY_PATH="$CURRENT_SCRIPT_DIR/acados/lib:${DYLD_LIBRARY_PATH}"
 else
-  export LD_LIBRARY_PATH="$CURRENT_SCRIPT_DIR/acados/lib"
+  export LD_LIBRARY_PATH="$CURRENT_SCRIPT_DIR/acados/lib:${LD_LIBRARY_PATH}"
 fi
 export ACADOS_SOURCE_DIR="$CURRENT_SCRIPT_DIR/acados"
 source venv_pyrocketcraft/bin/activate
